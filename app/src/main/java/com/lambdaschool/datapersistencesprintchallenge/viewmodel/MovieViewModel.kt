7c0fede7cda342f62bc78ed2fb8b038d170a68a5
msmaitran <itsmaisn@gmail.com>
2019-09-20 13:55:55 -0700

    Adding MovieViewModel

---
+++ app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/viewmodel/MovieViewModel.kt
@@ -0,0 +1,25 @@
+package com.lambdaschool.datapersistencesprintchallenge.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.lambdaschool.datapersistencesprintchallenge.model.FavoriteMovie
+
+class MovieViewModel (application: Application ): AndroidViewModel(application) {
+
+    fun getFavoriteMovies():  {
+        return
+    }
+
+    fun insert(movie: FavoriteMovie) {
+
+    }
+
+    fun update(movie: FavoriteMovie) {
+
+    }
+
+    fun delete(movie: FavoriteMovie) {
+
+    }
+
+}
