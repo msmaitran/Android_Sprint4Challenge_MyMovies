a7de69f62acb7e3636e633c25aa36a4379f2033b
msmaitran <itsmaisn@gmail.com>
2019-09-20 14:58:03 -0700

    Updated MovieRepository file with LiveData & AsyncTask.

--- app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/MovieRepository.kt
+++ app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/MovieRepository.kt
@@ -1,6 +1,7 @@
 package com.lambdaschool.datapersistencesprintchallenge
 
 import android.content.Context
+import android.os.AsyncTask
 import androidx.lifecycle.LiveData
 import com.lambdaschool.datapersistencesprintchallenge.database.MovieDAO
 import com.lambdaschool.datapersistencesprintchallenge.database.MovieDB
@@ -18,5 +19,43 @@ class MovieRepository(context: Context) {
         movieList = movieDao.getAllMovies()
     }
 
+    fun getAllMovies(): LiveData<List<FavoriteMovie>> {
+        return movieList
+    }
 
+    fun insert(movie: FavoriteMovie) {
+        val insertMovieAsyncTask = InsertMovieAsyncTask(movieDao).execute(movie)
+    }
+
+    fun update(movie: FavoriteMovie) {
+        val updateMovieAsyncTask = UpdateMovieAsyncTask(movieDao).execute(movie)
+    }
+
+    fun delete(movie: FavoriteMovie) {
+        val deleteMovieAsyncTask = DeleteMovieAsyncTask(movieDao).execute(movie)
+    }
+
+    companion object {
+
+        private class InsertMovieAsyncTask(movieDAO: MovieDAO) : AsyncTask<FavoriteMovie, Unit, Unit>() {
+            override fun doInBackground(vararg p0: FavoriteMovie?) {
+                MovieDAO.insert(p0[0]!!)
+            }
+            val MovieDAO = movieDAO
+        }
+
+        private class UpdateMovieAsyncTask(movieDAO: MovieDAO) : AsyncTask<FavoriteMovie, Unit, Unit>() {
+            override fun doInBackground(vararg p0: FavoriteMovie?) {
+                MovieDAO.update(p0[0]!!)
+            }
+            val MovieDAO = movieDAO
+        }
+
+        private class DeleteMovieAsyncTask(movieDAO: MovieDAO) : AsyncTask<FavoriteMovie, Unit, Unit>() {
+            override fun doInBackground(vararg p0: FavoriteMovie?) {
+                MovieDao.delete(p0[0]!!)
+            }
+            val MovieDao = movieDAO
+        }
+    }
 }
