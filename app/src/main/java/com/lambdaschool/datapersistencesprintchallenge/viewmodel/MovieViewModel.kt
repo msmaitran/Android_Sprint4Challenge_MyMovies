0164ed432bc06e74d28f189763015a1e1ea52d9b
msmaitran <itsmaisn@gmail.com>
2019-09-20 15:04:15 -0700

    Setting up MovieViewModel

--- app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/viewmodel/MovieViewModel.kt
@@ -2,24 +2,30 @@ package com.lambdaschool.datapersistencesprintchallenge.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.lambdaschool.datapersistencesprintchallenge.MovieRepository
 import com.lambdaschool.datapersistencesprintchallenge.model.FavoriteMovie
 
 class MovieViewModel (application: Application ): AndroidViewModel(application) {
 
-    fun getFavoriteMovies():  {
-        return
+    private var repository: MovieRepository = MovieRepository(application)
+
+    private var movieList: LiveData<List<FavoriteMovie>> = repository.getAllMovies()
+
+    fun getAllMovies(): LiveData<List<FavoriteMovie>> {
+        return movieList
     }
 
     fun insert(movie: FavoriteMovie) {
-
+        repository.insert(movie)
     }
 
     fun update(movie: FavoriteMovie) {
-
+        repository.update(movie)
     }
 
     fun delete(movie: FavoriteMovie) {
-
+        repository.delete(movie)
     }
 
 }
