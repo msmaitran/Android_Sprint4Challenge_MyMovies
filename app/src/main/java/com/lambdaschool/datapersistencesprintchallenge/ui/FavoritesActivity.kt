2363b7b1d46bc9ca2d32b66dae14e2834e71ab51
msmaitran <itsmaisn@gmail.com>
2019-09-20 08:36:10 -0700

    Created FavoritesActivity and added button functionality.

---
+++ app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/ui/FavoritesActivity.kt
@@ -0,0 +1,13 @@
+package com.lambdaschool.datapersistencesprintchallenge.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lambdaschool.datapersistencesprintchallenge.R
+
+class FavoritesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorites)
+    }
+}
--- app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/ui/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/ui/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.lambdaschool.datapersistencesprintchallenge.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lambdaschool.datapersistencesprintchallenge.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_favorites.setOnClickListener {
+            val intent = Intent(this, FavoritesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
